74dc4059c56a06c1ed57256a8940c2bc69f89d69
nellka <nel1@mail.ru>
2015-11-04 22:19:52 +0400

    верстка

--- pohoroshela.ru/mg-templates/pohoroshela/views/product.php
+++ pohoroshela.ru/mg-templates/pohoroshela/views/product.php
@@ -17,23 +17,11 @@ mgSEO($data);
      </div>
       <div id="tab1" itemprop="description" style="padding: 0 20px 0 0;text-align: justify;"><?php echo $data['description']?></div>
        <div class="clear"></div>
-       <div class="product-status-desc">Если вы хотите <font color="Red">легко</font> и <font color="Red">без проблем</font> сшить эту модель, <br>купите <font color="Red">готовый комплект</font> для ее пошива.<br>
+       <div class="product-status-desc">Если вы хотите <font color="Red">легко</font> и <font color="Red">без проблем</font> сшить эту модель, <br>купите <font color="Red" class="italic">готовый комплект</font> для ее пошива.<br>
        В набор входит:        
-       <div class="tk">* Ткань на выбор (3 метра стандартной ширины, цена комплекта зависит от ткани):</div>
+       <div class="tk">* Ткань на выбор (<font class="italic">3 метра стандартной ширины, цена комплекта зависит от ткани</font>):</div>
        </div>
-        <!-- <div class="product-status">
-       
-   
-<b>Набор для шитья:</b>
-<ul class="det"><li> инструкция
-<li> 3 метра ткани на выбор
-<li> склееная выкройка с размерами
-<li> пуговицы
-<li> нитки
-<li> молнии
-<li> подарочная упаковка
-</ul>
-</div>-->
+      
   <div class="product-status">
     <div class="buy-block">
       <div class="buy-block-inner">
@@ -46,6 +34,27 @@ mgSEO($data);
 
         <!--Кнопка, кототорая меняет свое значение с "В корзину" на "Подробнее"-->
         <?php echo $data['propertyForm'] ?>
+          <div class="product-status">
+       
+   <div class="status-desc">
+   <div class="sub-l">
+<ul><li>* инструкция по выкройке и пошиву этой модели
+<li>* склееная выкройка с размерами
+<li>* пуговицы в цвет ткани
+<li>* нитки в цвет ткани
+<li>* молнии в цвет ткани
+<li>* подарочная упаковка
+</ul>
+<img src="http://pohoroshela.ru/uploads/korbig.jpg" width="300">
+
+<div class="product-status-desc">Так же такой набор - отличный <font color="Red">Подарок!</font></div>
+</div>
+<div class="sub-r">
+<img src="http://pohoroshela.ru/uploads/instruc2.jpg" width="300">Пример инструкции из набора</div>
+</div>
+</div>
+ <div class="clear"></div>
+
         <div class="product-price mobile">
           <ul class="product-status-list">
             <li>
@@ -65,8 +74,8 @@ mgSEO($data);
   </div><!-- End product-status-->
 
   <div class="clear"></div>
-<div class="product-status-desc">
-А если вы <font color="Red">уверены в своих силах</font> - можете просто <font color="Red">скачать выкройку!</font><br>
+<div class="product-status-desc" style="padding: 20px 0 0;">
+А если вы <font color="Red">уверены в своих силах</font> - можете просто <font color="Red">скачать выкройку!</font><br><br>
           <?      
      if( file_exists(URL::getDocumentRoot().'/pdf/'.$data['id'].".zip")){?>
         <a class = "vikroika_url_pr" href = "<?php echo SITE.'/pdf/'.$data['id'].".zip" ?>">Скачать выкройку</a>
